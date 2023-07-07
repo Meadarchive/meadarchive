be684a915d06546f37d094dc63304d8afb4ef8c3
CharlieGroves <ctrg04@gmail.com>
2023-07-07 19:13:23 +0100

    signout and dymanically updating header based on auth state

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -1,15 +1,19 @@
 import SignIn from "../auth/SignIn";
+import SignOut from "../auth/SignOut";
+import { useAuth } from "../hooks/useAuth";
 import "./styles/header.css";
 
 export default function Header() {
+	const auth = useAuth();
+	const user = auth.user;
+	console.log(user);
 	return (
 		<div id="header-container">
 			<div id="header-logo">Logo here</div>
 			<div id="header-browse">Browse </div>
 			<div id="header-create-recipe">Create recipe</div>
-            <div id="header-see-batches">My batches</div>
-            <SignIn />
-            {/* <div id="header-auth">Sign in with google button here</div> */}
+			<div id="header-see-batches">My batches</div>
+			{user ? <SignOut /> : <SignIn />}
 		</div>
 	);
 }
