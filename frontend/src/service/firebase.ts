e562fb7c3bf2bdc5fc2c2912a3ceede2155f0f9b
CharlieGroves <ctrg04@gmail.com>
2023-07-07 18:51:06 +0100

    added firebase as dependancy

---
+++ frontend/src/service/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCCQtFabR1n3NJfTc_pqmYIiCssJ3KCQr4",
+	authDomain: "meadarchive.firebaseapp.com",
+	projectId: "meadarchive",
+	storageBucket: "meadarchive.appspot.com",
+	messagingSenderId: "323757239186",
+	appId: "1:323757239186:web:154f5ffe989af49507ef4b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
