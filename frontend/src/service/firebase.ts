9e5673a0335d74c58b756deb85ee6686bcc190a1
CharlieGroves <ctrg04@gmail.com>
2023-07-07 18:55:38 +0100

    connected to firebase

--- frontend/src/service/firebase.ts
+++ frontend/src/service/firebase.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCCQtFabR1n3NJfTc_pqmYIiCssJ3KCQr4",
-	authDomain: "meadarchive.firebaseapp.com",
-	projectId: "meadarchive",
-	storageBucket: "meadarchive.appspot.com",
-	messagingSenderId: "323757239186",
-	appId: "1:323757239186:web:154f5ffe989af49507ef4b",
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export default firebase;
