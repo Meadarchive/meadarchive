c1f6c5b679ab017c751689595305bfa59d57296d
CharlieGroves <ctrg04@gmail.com>
2023-07-07 16:29:54 +0100

    global header templated

---
+++ frontend/src/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./styles/header.css";
+
+export default function Header() {
+	return (
+		<div id="header-container">
+			<div id="header-logo">Logo here</div>
+			<div id="header-browse">Browse </div>
+			<div id="header-create-recipe">Create recipe</div>
+            <div id="header-see-batches">My batches</div>
+            <div id="header-auth">Sign in with google button here</div>
+		</div>
+	);
+}
