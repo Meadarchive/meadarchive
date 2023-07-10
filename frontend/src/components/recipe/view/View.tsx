cfaa5142c283d622f3a1a3da124cc4b9ac7651b5
CharlieGroves <ctrg04@gmail.com>
2023-07-10 18:27:13 +0100

    recipe object dumping to screen

--- frontend/src/components/recipe/view/View.tsx
+++ frontend/src/components/recipe/view/View.tsx
@@ -1,29 +1,84 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import CreateFormState from "../create/interfaces/CreateFormState";
+import RecipeInterface from "./interfaces/RecipeInterface";
 
 export default function View() {
 	// get recipe id from url
 	let params = useParams();
-	let rid = params.rid;
-	console.log(rid);
+	let rid:string = params.rid ? params.rid : "";
 
-	const [recipe, setRecipe] = useState({} as CreateFormState);
+	const [recipe, setRecipe] = useState<RecipeInterface>();
 
 	useEffect(() => {
 		// get recipe from backend
 		(async () => {
-			console.log("test")
 			let res = await fetch(
 				`${import.meta.env.VITE_SERVER_URL}/recipe?recipeID=${rid}`
 			);
 			let data = await res.json();
-			console.log(data);
-			setRecipe(data);
+			setRecipe(data.recipes[rid]);
 		})();
-		console.log(recipe);
 	}, []);
 
 	// get recipe from backend
-	return <div>recipe view</div>;
+	return (
+		<div>
+			{recipe ? (
+				<div>
+					<p>Author: {recipe.author}</p>
+					<h1>{recipe.recipeName}</h1>
+					<p>Yeast Type: {recipe.yeastType}</p>
+					<p>
+						Recipe Size: {recipe.recipeSize} {recipe.recipeSizeUnit}
+					</p>
+					<p>Recipe Description: {recipe.recipeDescription}</p>
+
+					<h2>Honey Types</h2>
+					{recipe.honeyTypes.map((honeyType, index) => (
+						<div key={index}>
+							<p>
+								Amount: {honeyType.amount} {honeyType.unit}
+							</p>
+							<p>Honey: {honeyType.honey}</p>
+						</div>
+					))}
+
+
+					<h2>Liquids</h2>
+					{recipe.liquids.map((liquid, index) => (
+						<div key={index}>
+							<p>
+								Amount: {liquid.amount} {liquid.unit}
+							</p>
+							<p>Liquid: {liquid.liquid}</p>
+						</div>
+					))}
+
+					<h2>Chemicals</h2>
+					{recipe.chemicals.length > 0 ? (
+						<ul>
+							{recipe.chemicals.map((chemical, index) => (
+								<li key={index}>{chemical.chemical}</li>
+							))}
+						</ul>
+					) : (
+						<p>No chemicals used</p>
+					)}
+
+					<h2>Addons</h2>
+					{recipe.addons.length > 0 ? (
+						<ul>
+							{recipe.addons.map((addon, index) => (
+								<li key={index}>{addon.addon}</li>
+							))}
+						</ul>
+					) : (
+						<p>No chemicals used</p>
+					)}
+				</div>
+			) : (
+				<div>loading</div>
+			)}
+		</div>
+	);
 }
---
+++ frontend/src/components/recipe/view/interfaces/RecipeInterface.ts
@@ -0,0 +1,5 @@
+import CreateFormState from '../../create/interfaces/CreateFormState';
+
+export default interface RecipeInterface extends CreateFormState {
+	author: string;
+}
