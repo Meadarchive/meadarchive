90692502830a64be87178d84b92f2d19c4d979a7
CharlieGroves <ctrg04@gmail.com>
2023-07-23 17:39:19 +0100

    abstracted DashboardRecipe into its own functional component to make files more manageable

---
+++ frontend/src/components/dashboard/DashboardRecipe.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import RecipeInterface from "../recipe/view/interfaces/RecipeInterface";
+import "./styles/dashboard.css";
+import recipeSizeFormatter from "../recipe/view/helpers/recipeSizeFormatter";
+import DeleteConfirmation from "../recipe/view/DeleteConfirmation";
+import deleteRecipe from "../recipe/view/helpers/deleteRecipe";
+import firebase from "../../service/firebase";
+
+interface DashboardRecipeProps {
+	recipe: RecipeInterface;
+	user: firebase.User | null;
+	rid: string;
+}
+
+const DashboardRecipe: React.FC<DashboardRecipeProps> = ({
+	recipe,
+	user,
+	rid,
+}) => {
+	const handleDeleteRecipe = async (
+		rid: string,
+		user: firebase.User | null
+	) => {
+		user && (await deleteRecipe(rid, user));
+		// You might want to refresh the list of recipes after deleting
+		// For example, fetch the updated list of recipes from the backend and update the state accordingly
+	};
+
+	return (
+		<div className="dashboard-recipe-container">
+			<div className="dashboard-recipe">
+				<div className="dashboard-recipe-left">
+					<div className="dashboard-recipe-title">
+						<Link to={`/recipe/${rid}`}>{recipe.recipeName}</Link>
+					</div>
+					<div className="dashboard-recipe-yeast">
+						{recipe.yeastType} {recipe.yeastAmount}g
+					</div>
+					<div className="dashboard-recipe-size">
+						{recipeSizeFormatter(
+							recipe.recipeSizeUnit,
+							recipe.recipeSize
+						)}
+					</div>
+				</div>
+				<div className="dashboard-delete-recipe">
+					{user && (
+						<DeleteConfirmation
+							onConfirm={() => handleDeleteRecipe(rid, user)}
+						/>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DashboardRecipe;
