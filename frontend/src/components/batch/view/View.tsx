b6c203df5389b34003539fafb3035a409daed9c4
CharlieGroves <ctrg04@gmail.com>
2023-07-25 21:08:58 +0100

    batch info cleaned up

--- frontend/src/components/batch/view/View.tsx
+++ frontend/src/components/batch/view/View.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import getBatchByBID from "../../../api/get/getBatchByBID";
 import { BatchWithUpdates } from "../../../api/interfaces/batchInterface";
 import UpdatesContainer from "./UpdatesContainer";
+import BatchInfo from "./BatchInfo";
+import EquipmentList from "./EquipmentList";
 import "./styles/view.css";
 
 export default function View() {
@@ -27,33 +29,8 @@ export default function View() {
 				<>
 					<div id="batch-created-title">Batch Created</div>
 					<div id="starting-container">
-						<div className="batch-info">
-							<div>Stage: {batchInfo.stage}</div>
-							<div>Date Started: {batchInfo.dateStarted}</div>
-							<div>
-								Initial Gravity: {batchInfo.initialGravity}
-							</div>
-							<div>Author: {batchInfo.author}</div>
-							<div>Water: {batchInfo.water}</div>
-							<div id="equipment-title">Equipment:</div>
-							<div className="equipment-list">
-								{batchInfo.equipment &&
-									batchInfo.equipment.map((item, index) => (
-										<div key={index}>
-											{Object.entries(item).map(
-												([key, value], index) => (
-													<div
-														className="equipment-item"
-														key={index}
-													>
-														{key}: {value}
-													</div>
-												)
-											)}
-										</div>
-									))}
-							</div>
-						</div>
+						<BatchInfo batchInfo={batchInfo} />
+						<EquipmentList equipment={batchInfo.equipment} />
 					</div>
 				</>
 			)}
