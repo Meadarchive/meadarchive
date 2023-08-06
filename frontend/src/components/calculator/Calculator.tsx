e062ee7de40c35389d4e0c6a53be776789b9e6ea
CharlieGroves <ctrg04@gmail.com>
2023-08-06 18:14:39 +0100

    Calcualtor with basic validation

--- frontend/src/components/calculator/Calculator.tsx
+++ frontend/src/components/calculator/Calculator.tsx
@@ -1,5 +1,88 @@
+import React, { useEffect, useState } from "react";
 import "./styles/calculator.css";
 
-export default function Calculator() {
-	return <div>Calculator</div>;
-}
+const Calculator: React.FC = () => {
+	const [originalGravity, setOriginalGravity] = useState<number>(1.0);
+	const [finalGravity, setFinalGravity] = useState<number>(1.0);
+	const [error, setError] = useState<string>("");
+	const [calculatedABV, setCalculatedABV] = useState<number | undefined>(
+		undefined
+	);
+
+	const calculateABV = (og: number, fg: number): void => {
+		if (og >= 0.9 && og <= 2 && fg >= 0.9 && fg <= 2) {
+			setCalculatedABV((og - fg) * 131.25);
+			setError("");
+		} else if (og < fg) {
+			setError("Final gravity must be less than original gravity");
+			setCalculatedABV(undefined);
+		} else {
+			setError("Gravity must be between 0.9 and 2");
+			setCalculatedABV(undefined);
+		}
+	};
+
+    useEffect(() => {
+        calculateABV(originalGravity, finalGravity);
+    },[])
+
+	const handleOriginalGravityChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const value = parseFloat(e.target.value);
+		setOriginalGravity(value);
+		calculateABV(value, finalGravity); // Call calculateABV when originalGravity changes
+	};
+
+	const handleFinalGravityChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const value = parseFloat(e.target.value);
+		setFinalGravity(value);
+		calculateABV(originalGravity, value); // Call calculateABV when finalGravity changes
+	};
+
+	return (
+		<div id="calculator-container">
+			<div id="calculator-subcontainer">
+				<div id="calculator-title">ABV from hydrometer</div>
+				<div id="calculator">
+					<div id="og" className="gravity-container">
+						<label className="gravity-label">
+							Original Gravity:
+						</label>
+						<input
+							className="gravity-input"
+							type="number"
+							step="0.01"
+							value={originalGravity}
+							onChange={handleOriginalGravityChange}
+						/>
+					</div>
+					<div className="gravity-container">
+						<label className="gravity-label">Final Gravity:</label>
+						<input
+							className="gravity-input"
+							type="number"
+							step="0.01"
+							value={finalGravity}
+							onChange={handleFinalGravityChange}
+						/>
+					</div>
+					<div id="subtext">
+						{calculatedABV !== undefined ? (
+							<p>
+								The ABV of your brew is{" "}
+								{calculatedABV.toFixed(2)}%
+							</p>
+						) : (
+							<p>{error}</p>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Calculator;
--- frontend/src/components/homepage/Homepage.tsx
+++ frontend/src/components/homepage/Homepage.tsx
@@ -7,36 +7,39 @@ import Calculator from "../calculator/Calculator";
 export default function Homepage() {
 	const { user } = useAuth();
 	return (
-		<div id="welcome-container">
-			<div id="welcome">
-				{user ? (
-					<div className="bold">
-						Welcome to meadarchive {user.displayName}!
-					</div>
-				) : (
-					<div className="bold">Welcome to meadarchive!</div>
-				)}
-				<div>
-					Meadarchive is a passion project to create a way to preserve and share
-					mead recipes and brews. It is currently in very early alpha
-					version as a proof of concept so many features do not exist
-					and some may be broken
-				</div>
-				<div>Take your first steps:</div>
-				<ul>
-					{!user && (
-						<>
-							<div>Sign in below</div>
-						</>
+		<>
+			<div id="welcome-container">
+				<div id="welcome">
+					{user ? (
+						<div className="bold">
+							Welcome to meadarchive {user.displayName}!
+						</div>
+					) : (
+						<div className="bold">Welcome to meadarchive!</div>
 					)}
-					{!user && <Auth>Sign in </Auth>}
 					<div>
-						<Link to="/recipe/create">Create a recipe</Link>
+						Meadarchive is a passion project to create a way to
+						preserve and share mead recipes and brews. It is
+						currently in very early alpha version as a proof of
+						concept so many features do not exist and some may be
+						broken
 					</div>
-					<div>Take notes of a batch</div>
-				</ul>
+					<div>Take your first steps:</div>
+					<ul>
+						{!user && (
+							<>
+								<div>Sign in below</div>
+							</>
+						)}
+						{!user && <Auth>Sign in </Auth>}
+						<div>
+							<Link to="/recipe/create">Create a recipe</Link>
+						</div>
+						<div>Take notes of a batch</div>
+					</ul>
+				</div>
 			</div>
 			<Calculator />
-		</div>
+		</>
 	);
 }
