b91d62bf4bb3a41b1c6a5bcef51f341f9179a383
CharlieGroves <ctrg04@gmail.com>
2023-08-08 20:43:30 +0100

    updated both deletes

--- frontend/src/api/delete/deleteBatch.ts
+++ frontend/src/api/delete/deleteBatch.ts
@@ -1,7 +1,9 @@
 import { getDeleteBatchOptions } from "../options";
 import firebase from "../../service/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default async function deleteBatch(bid: string, user: firebase.User) {
+	const navigate = useNavigate()
 	// delete Batch from backend
 	let res = await fetch(
 		`${import.meta.env.VITE_SERVER_URL}/batch/delete`,
@@ -10,6 +12,6 @@ export default async function deleteBatch(bid: string, user: firebase.User) {
 
 	// if Batch was deleted, redirect to home page
 	if (res.status === 200) {
-		window.location.href = "/dashboard";
+		navigate("/dashboard");
 	}
 }
