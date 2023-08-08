45851d7ea1f6c3975b6320933c05d2a4633032f7
CharlieGroves <ctrg04@gmail.com>
2023-08-08 22:54:03 +0100

    Refactor: Improve Readability and Clean Code in View Component

--- frontend/src/components/batch/view/View.tsx
+++ frontend/src/components/batch/view/View.tsx
@@ -7,16 +7,19 @@ import BatchInfo from "./BatchInfo";
 import EquipmentList from "./EquipmentList";
 import "./styles/view.css";
 
+interface Params {
+	bid?: string;
+}
+
 export default function View() {
-	const params = useParams<{ bid?: string }>();
-	const bid = params.bid || "";
+	const { bid = "" } = useParams<Params>();
 	const navigate = useNavigate();
 
 	const [batchInfo, setBatchInfo] = useState<BatchWithUpdates | null>(null);
 
 	useEffect(() => {
 		const fetchBatchInfo = async () => {
-			console.log("fetching batch info for " + bid);
+			console.log(`Fetching batch info for ${bid}`);
 			const recipeInfo = await getBatchByBID(bid);
 			setBatchInfo(recipeInfo);
 		};
@@ -24,20 +27,28 @@ export default function View() {
 		fetchBatchInfo();
 	}, [bid]);
 
+	const renderBatchInfo = () => (
+		<>
+			<div id="batch-created-title">Batch Created</div>
+			<div className="starting-container">
+				<BatchInfo batchInfo={batchInfo!} />
+				<EquipmentList equipment={batchInfo!.equipment} />
+			</div>
+		</>
+	);
+
 	return (
 		<div id="view-batch-container">
-			{batchInfo && (
-				<>
-					<div id="batch-created-title">Batch Created</div>
-					<div className="starting-container">
-						<BatchInfo batchInfo={batchInfo} />
-						<EquipmentList equipment={batchInfo.equipment} />
-					</div>
-				</>
-			)}
+			{batchInfo && renderBatchInfo()}
 
 			<UpdatesContainer batchInfo={batchInfo} />
-			<button onClick={() => navigate(`/batch/update/${bid}`)} id="add-update-button">Add Update</button>
+
+			<button
+				onClick={() => navigate(`/batch/update/${bid}`)}
+				id="add-update-button"
+			>
+				Add Update
+			</button>
 		</div>
 	);
 }
