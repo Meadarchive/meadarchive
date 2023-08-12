6aece4344adc603045d5987e324a71847c7a3cc5
CharlieGroves <ctrg04@gmail.com>
2023-08-12 18:20:02 +0100

    removed unused variable assignment  'res' is declared but its value is never read.

--- frontend/src/components/batch/view/BatchUpdate.tsx
+++ frontend/src/components/batch/view/BatchUpdate.tsx
@@ -78,7 +78,7 @@ export default function Update() {
 			};
 		}
 
-		const res = await createBatchUpdate(auth.user, parsedBatchUpdate);
+		await createBatchUpdate(auth.user, parsedBatchUpdate);
 		navigate(`/batch/${batchUpdate.batchID}`);
 	};
 
