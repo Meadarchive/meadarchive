fe6e61d8e951bb36a869c364f77efd45e515f4c4
CharlieGroves <ctrg04@gmail.com>
2023-08-14 20:18:46 +0100

    centered user metrics

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -7,6 +7,8 @@ import getUserByUID from "../../api/get/getUserByUID";
 import getRecipesByUID from "../../api/get/getRecipesByUID";
 import getBatchesByUID from "../../api/get/getBatchesByUID";
 import CountRecipes from "./CountRecipes";
+import CountBatches from "./CountBatches";
+import "./styles/profile.css"
 
 export default function Profile() {
 	const [user, setUser] = useState({} as firebase.User);
@@ -20,63 +22,25 @@ export default function Profile() {
 			const user = await getUserByUID(uid);
 			const recipes = await getRecipesByUID(uid);
 			const batches = await getBatchesByUID(uid);
-	
+
 			setUser(user.userInfo);
 			setUserRecipes(recipes);
 			setUserBatches(batches);
 		};
-	
+
 		fetchData();
 	}, []);
-	
-
-	useEffect(() => {
-		console.log(user);
-		console.log(userRecipes);
-		console.log(userBatches);
-	}, [user, userRecipes, userBatches]);
 
 	return (
 		<div>
 			{user && user.displayName ? (
-				<>
-					<div>{user.displayName}</div>
+				<div className="user-info-container">
+					<div className="bold">{user.displayName}</div>
 					{userRecipes && <CountRecipes userRecipes={userRecipes} />}
-					{userBatches && <div>{userBatches.length} batches</div>}
-				</>
+					{userBatches && <CountBatches userBatches={userBatches} />}
+				</div>
 			) : (
 				<div>No user data</div>
-			)}
-			{userRecipes && Object.entries(userRecipes).length > 0 ? (
-				<h2>Recipes</h2>
-			) : (
-				<></>
-			)}
-			{userRecipes && Object.entries(userRecipes).length > 0 ? (
-				Object.entries(userRecipes).map(([_, recipe], index) => {
-					return (
-						<div key={index}>
-							<h3>{recipe.recipeName}</h3>
-							<p>{recipe.recipeDescription}</p>
-						</div>
-					);
-				})
-			) : (
-				<div>No recipe data</div>
-			)}
-			{userBatches && userBatches.length > 0 ? <h2>Batches</h2> : <></>}
-			{userBatches && Object.entries(userBatches).length > 0 ? (
-				Object.entries(userBatches).map(([_, batch], index) => {
-					console.log(batch);
-					return (
-						<div key={index}>
-							<h3>{batch.batchName}</h3>
-							<p>{batch.dateStarted}</p>
-						</div>
-					);
-				})
-			) : (
-				<div>No batch data</div>
 			)}
 		</div>
 	);
