d3db5d6359929a9787cf0e744874112e06e4966f
CharlieGroves <ctrg04@gmail.com>
2023-08-14 20:44:40 +0100

    showing recipes and batches

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import firebase from "../../service/firebase";
 import { useParams } from "react-router-dom";
-import { Batch } from "../../api/interfaces/batchInterface";
+import { Batch, BatchWithUpdates } from "../../api/interfaces/batchInterface";
 import RecipeInterface from "../recipe/view/interfaces/RecipeInterface";
 import getUserByUID from "../../api/get/getUserByUID";
 import getRecipesByUID from "../../api/get/getRecipesByUID";
@@ -9,11 +9,15 @@ import getBatchesByUID from "../../api/get/getBatchesByUID";
 import CountRecipes from "./CountRecipes";
 import CountBatches from "./CountBatches";
 import "./styles/profile.css";
+import DashboardRecipe from "../dashboard/DashboardRecipe";
+import { useAuth } from "../../hooks/useAuth";
+import DashboardBatchInfo from "../dashboard/DashboardBatchInfo";
 
 export default function Profile() {
-	const [user, setUser] = useState({} as firebase.User);
+	const [userInfo, setUserInfo] = useState({} as firebase.User);
 	const [userRecipes, setUserRecipes] = useState([] as RecipeInterface[]);
 	const [userBatches, setUserBatches] = useState([] as Batch[]);
+	const { user } = useAuth();
 	const params = useParams();
 	const uid = params.uid ? params.uid : "";
 
@@ -23,7 +27,7 @@ export default function Profile() {
 			const recipes = await getRecipesByUID(uid);
 			const batches = await getBatchesByUID(uid);
 
-			setUser(user.userInfo);
+			setUserInfo(user.userInfo);
 			setUserRecipes(recipes);
 			setUserBatches(batches);
 		};
@@ -33,22 +37,56 @@ export default function Profile() {
 
 	return (
 		<div>
-			{user && user.displayName ? (
+			{userInfo && userInfo.displayName ? (
 				<div className="user-info-container">
-					{user.photoURL && (
+					{userInfo.photoURL && (
 						<img
 							referrerPolicy="no-referrer"
-							src={user.photoURL}
-							alt={user.displayName}
+							src={userInfo.photoURL}
+							alt={userInfo.displayName}
 							className="profile-photo"
 						/>
 					)}
-					<div className="bold">{user.displayName}</div>
+					<div className="bold-and-bigger">
+						{userInfo.displayName}
+					</div>
 					{userRecipes && <CountRecipes userRecipes={userRecipes} />}
 					{userBatches && <CountBatches userBatches={userBatches} />}
 				</div>
 			) : (
 				<div>No user data</div>
+			)}
+			{userRecipes && Object.entries(userRecipes).length > 0 && (
+				<div className="user-recipes-container">
+					<div className="user-recipes-title bold-and-bigger">
+						Recipes
+					</div>
+					{Object.entries(userRecipes).map(([rid, recipe]) => {
+						return (
+							<DashboardRecipe
+								user={user}
+								rid={rid}
+								recipe={recipe}
+							/>
+						);
+					})}
+				</div>
+			)}
+			{userBatches && Object.entries(userBatches).length > 0 && (
+				<>
+					<div className="user-batches-title bold-and-bigger">
+						Batches
+					</div>
+					<div className="user-batches-container">
+						{Object.entries(userBatches).map(([_, batch]) => {
+							return (
+								<DashboardBatchInfo
+									batchInfo={batch as BatchWithUpdates}
+								/>
+							);
+						})}
+					</div>
+				</>
 			)}
 		</div>
 	);
